44816d09397a7f38fe3ffe8542a759deb657ff89
Nambinintsoa Chrysologue <chrysologuenambinintsoa@gmail.com>
2026-09-06 01:59:58 +0300

    Mark root layout as dynamic

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,6 +22,8 @@ export const viewport = {
   themeColor: "#EFF4F9",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({ children }) {
   let session = null;
   try {
