f9649d5a8dd793c8c768842331e6742b3dc14403
Nambinintsoa Chrysologue <chrysologuenambinintsoa@gmail.com>
2026-09-10 23:41:21 +0300

    fix: android startup splash and offline fallback

--- android/app/src/main/java/com/lynoralink/app/MainActivity.java
+++ android/app/src/main/java/com/lynoralink/app/MainActivity.java
@@ -1,46 +1,35 @@
 package com.lynoralink.app;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.os.Build;
 import android.os.Bundle;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.core.splashscreen.SplashScreen;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-	private boolean keepSplashVisible = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
-		splashScreen.setKeepOnScreenCondition(() -> keepSplashVisible);
-
 		super.onCreate(savedInstanceState);
-
-		if (this.bridge != null && this.bridge.getWebView() != null) {
-			this.bridge.getWebView().setWebViewClient(new OfflineAwareWebViewClient());
-		}
-
 		registerConnectivityMonitoring();
 
 		if (!hasInternetConnection()) {
-			keepSplashVisible = true;
 			Toast.makeText(this, "Connexion Internet indisponible", Toast.LENGTH_LONG).show();
-		} else {
-			keepSplashVisible = false;
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (!hasInternetConnection()) {
+			if (bridge != null && bridge.getWebView() != null) {
+				bridge.getWebView().loadUrl("file:///android_asset/offline.html");
+			}
 		}
 	}
 
@@ -54,9 +43,8 @@ public class MainActivity extends BridgeActivity {
 				@Override
 				public void onAvailable(@NonNull Network network) {
 					runOnUiThread(() -> {
-						keepSplashVisible = false;
-						if (thisBridgeHasOfflinePage()) {
-							if (bridge != null && bridge.getWebView() != null) {
+						if (bridge != null && bridge.getWebView() != null) {
+							if ("file:///android_asset/offline.html".equals(bridge.getWebView().getUrl())) {
 								bridge.getWebView().reload();
 							}
 						}
@@ -65,14 +53,14 @@ public class MainActivity extends BridgeActivity {
 
 				@Override
 				public void onLost(@NonNull Network network) {
-					runOnUiThread(() -> keepSplashVisible = true);
+					runOnUiThread(() -> {
+						if (bridge != null && bridge.getWebView() != null) {
+							bridge.getWebView().loadUrl("file:///android_asset/offline.html");
+						}
+					});
 				}
 			});
 		}
-	}
-
-	private boolean thisBridgeHasOfflinePage() {
-		return bridge != null && bridge.getWebView() != null && "file:///android_asset/offline.html".equals(bridge.getWebView().getUrl());
 	}
 
 	private boolean hasInternetConnection() {
@@ -93,65 +81,5 @@ public class MainActivity extends BridgeActivity {
 
 		android.net.NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-	}
-
-	private class OfflineAwareWebViewClient extends WebViewClient {
-		@Override
-		public void onPageStarted(WebView view, String url, Bitmap favicon) {
-			super.onPageStarted(view, url, favicon);
-			if (!hasInternetConnection()) {
-				keepSplashVisible = true;
-				if (!"file:///android_asset/offline.html".equals(url)) {
-					view.stopLoading();
-					view.loadUrl("file:///android_asset/offline.html");
-				}
-			} else {
-				keepSplashVisible = false;
-			}
-		}
-
-		@Override
-		public void onPageFinished(WebView view, String url) {
-			super.onPageFinished(view, url);
-			if (!hasInternetConnection()) {
-				keepSplashVisible = true;
-				return;
-			}
-
-			if ("file:///android_asset/offline.html".equals(url)) {
-				keepSplashVisible = false;
-				return;
-			}
-
-			keepSplashVisible = false;
-		}
-
-		@Override
-		public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-			super.onReceivedError(view, request, error);
-			if (!hasInternetConnection()) {
-				keepSplashVisible = true;
-				if (!"file:///android_asset/offline.html".equals(view.getUrl())) {
-					view.stopLoading();
-					view.loadUrl("file:///android_asset/offline.html");
-				}
-			} else {
-				keepSplashVisible = false;
-			}
-		}
-
-		@Override
-		public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
-			super.onReceivedHttpError(view, request, errorResponse);
-			if (!hasInternetConnection()) {
-				keepSplashVisible = true;
-				if (!"file:///android_asset/offline.html".equals(view.getUrl())) {
-					view.stopLoading();
-					view.loadUrl("file:///android_asset/offline.html");
-				}
-			} else {
-				keepSplashVisible = false;
-			}
-		}
 	}
 }
