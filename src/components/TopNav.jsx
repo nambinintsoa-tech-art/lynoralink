f2d1c6fe971c72b29a4deeff4bc443dd4eed48d8
Nambinintsoa Chrysologue <chrysologuenambinintsoa@gmail.com>
2026-09-20 01:36:21 +0300

    Fix home navigation section reset

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -487,7 +487,8 @@ export const TopNav = forwardRef(function TopNav({
   }, [isReloading]);
 
   const goHome = () => {
-    triggerHardRefresh();
+    setMenuOpen(false);
+    onNavigate("feed");
   };
 
   useEffect(() => {
